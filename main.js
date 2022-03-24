f03de95001c038324e98dd58deb59dff57cfe612
Kayky Dias <kaykydiasw@outlook.com.br>
2022-03-23 22:38:42 -0300

    basic main page & side navigation

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+
+function OpenSideNav() {
+    document.getElementById("my-side-nav").style.width = "100%";
+}
+function CloseSideNav() {
+    document.getElementById("my-side-nav").style.width = "0";
+}
