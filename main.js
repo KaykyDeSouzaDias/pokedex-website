5792b84f83c0d37f896a73e213c5bfb8d1a6ee92
Kayky Dias <kaykydiasw@outlook.com.br>
2022-03-29 17:51:53 -0300

    pokedex system 0.1

--- main.js
+++ main.js
@@ -2,8 +2,38 @@
 
 
 function OpenSideNav() {
-    document.getElementById("my-side-nav").style.width = "100%";
+    /*document.getElementById("my-side-nav").style.width = "100%";*/
 }
 function CloseSideNav() {
     document.getElementById("my-side-nav").style.width = "0";
 }
+
+
+const fetchPokemon = () => {
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+    const pokemonPromises = [];
+
+    for (let i = 1; i <= 150; i++) {
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));
+    }
+
+    Promise.all(pokemonPromises)
+        .then(pokemons => {
+            console.log(pokemons)
+
+            const pokemonList = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+
+                accumulator += `
+                    <li>${pokemon.name}</li>
+                `;
+                return accumulator;
+            }, '');
+
+            //console.log(pokemonList)
+        });
+}
+
+fetchPokemon();
+
