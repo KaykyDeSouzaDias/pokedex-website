360839da7bec33c1dee9da852bcc8d9f88c5a929
Kayky Dias <kaykydiasw@outlook.com.br>
2022-03-29 21:08:34 -0300

    pokedex system 0.2

--- main.js
+++ main.js
@@ -2,38 +2,42 @@
 
 
 function OpenSideNav() {
-    /*document.getElementById("my-side-nav").style.width = "100%";*/
+    document.getElementById("my-side-nav").style.width = "100%";
 }
 function CloseSideNav() {
     document.getElementById("my-side-nav").style.width = "0";
 }
 
 
-const fetchPokemon = () => {
-    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-    const pokemonPromises = [];
 
-    for (let i = 1; i <= 150; i++) {
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));
-    }
+const createPokemonPromises = () => Array(150).fill().map((_, index) => fetch(getPokemonUrl(index + 1)).then(response => response.json()));
 
-    Promise.all(pokemonPromises)
-        .then(pokemons => {
-            console.log(pokemons)
+    
+const createPokemonsHTML = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
+    const pokemonTypes = types.map(typeInfo => typeInfo.type.name);
 
-            const pokemonList = pokemons.reduce((accumulator, pokemon) => {
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+    accumulator += `
+        <li class="pokemon-card ${pokemonTypes[0]}">
+            <img class="pokemon-card-image" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png">
+            <img class="pokemon-card-background" src="/imgs/backgrounds/pokeball_bg.png" alt="">
+            <h2 class="pokemon-card-title">${name}</h2>
+            <p class="pokemon-card-subtitle">${pokemonTypes.join(' | ')}</p>
+        </li>
+    `;
+    return accumulator;
+}, '');
 
-                accumulator += `
-                    <li>${pokemon.name}</li>
-                `;
-                return accumulator;
-            }, '');
 
-            //console.log(pokemonList)
-        });
+const insertPokemonsHTML = pokemons => {
+
+    const ul = document.querySelector('[data-js="pokedex"]');
+    ul.innerHTML = pokemons;
+
 }
 
-fetchPokemon();
+
+const pokemonPromises = createPokemonPromises();
+Promise.all(pokemonPromises).then(createPokemonsHTML).then(insertPokemonsHTML)
 
