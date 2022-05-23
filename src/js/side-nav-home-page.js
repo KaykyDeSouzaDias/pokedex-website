fea91d9b45b246b19628e47faf8dbe1e3e3c0d93
Kayky Dias <kaykydiasw@outlook.com.br>
2022-05-23 15:22:56 -0300

    - changing side nav of home page js file name

--- src/js/side-nav-home-page.js
+++ src/js/side-nav-home-page.js
@@ -7,8 +7,6 @@ function CloseSideNav() {
     document.getElementById("my-side-nav").style.width = "0";
 }
 
-
-
 const insertGenerationButtonImages = () => {
     for (let i = 1; i <= 8; i++) {
         document.getElementById(`generation-0${i}`).innerHTML = `<h1 style="text-decoration=none;">0${i}</h1><img src="imgs/generation-buttons/generation_0${i}.png">`;
